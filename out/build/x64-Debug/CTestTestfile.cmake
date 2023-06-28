fc30b14cd5b553e686685d10ffff48cda48f3a13
evorll <113104366+evorll@users.noreply.github.com>
2023-06-28 16:41:39 +0300

    Delete out directory

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Ivan/Downloads/Telegram Desktop/Project
-# Build directory: C:/Users/Ivan/Downloads/Telegram Desktop/Project/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
